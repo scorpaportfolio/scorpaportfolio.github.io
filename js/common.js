bb7472670917207661eb482fdd7e2774e4fe2a09
scorpaportfolio <49238663+scorpaportfolio@users.noreply.github.com>
2019-04-10 21:45:07 +0300

    ajax_in_index

--- js/common.js
+++ js/common.js
@@ -1,20 +1,5 @@
 // $(function() {
 
-	$(document).ready(function () {
-
-		$("#modal-body").submit(function () {
-			$.ajax({
-				type: "POST",
-				url: "../mail.php",
-				data: $(this).serialize()
-			}).done(function () {
-				$(this).find("input").val("");
-				alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
-				$("#modal-body").trigger("reset");
-			});
-			return false;
-		});
-
-	});
+	
 
 // });
