dc562ef243e655007e20ce04f8c86d1f0d804392
scorpaportfolio <49238663+scorpaportfolio@users.noreply.github.com>
2019-04-11 11:33:54 +0300

    js

--- js/common.js
+++ js/common.js
@@ -1,5 +1,20 @@
 // $(function() {
 
-	
+	$(document).ready(function () {
+
+		$("#modal-body").submit(function () {
+			$.ajax({
+				type: "POST",
+				url: "../mail.php",
+				data: $(this).serialize()
+			}).done(function () {
+				$(this).find("input").val("");
+				alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+				$("#modal-body").trigger("reset");
+			});
+			return false;
+		});
+
+	});
 
 // });
